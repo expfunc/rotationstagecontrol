71b651ac33304a69261b5122c37c37c0d55ec7e5
expfunc <dimakornuhin@mail.ru>
2024-07-22 16:01:00 +0300

    Feat: edited parse_command

--- command_parser.py
+++ command_parser.py
@@ -1,37 +1,37 @@
 import struct
 from standa import Standa
+from utils import Utils
 
 
 class CommandParser:
-    def __init__(self):
+    def __init__(self, udp_server):
         self.standa_device = Standa()
-        self.command_list_ids = {'0x0100': self.standa_device.connect,
-                                 '0x0101': self.standa_device.disconnect,
-                                 '0x0200': self.standa_device.move_absolute,
-                                 '0x0201': self.standa_device.move_relative,
-                                 '0x0202': self.standa_device.set_speed,
-                                 '0x0203': self.standa_device.set_acceleration,
-                                 '0x0204': self.standa_device.set_deceleration,
-                                 '0x0205': self.standa_device.set_zero,
-                                 '0x0206': self.standa_device.abort,
-                                 '0x0300': self.standa_device.get_current_position,
-                                 # '0x0301': self.standa_device.get_move_settings,
-                                 '0x0302': self.standa_device.get_status}
+        self.udp_server = udp_server
+        self.command_dict_ids = {0x0100: self.standa_device.connect,
+                                 0x0101: self.standa_device.disconnect,
+                                 0x0200: (self.standa_device.move_absolute, float),
+                                 0x0201: (self.standa_device.move_relative, float),
+                                 0x0202: (self.standa_device.set_speed, float),
+                                 0x0203: (self.standa_device.set_acceleration, float),
+                                 0x0204: (self.standa_device.set_deceleration, float),
+                                 0x0205: self.standa_device.set_zero,
+                                 0x0206: self.standa_device.abort,
+                                 0x0300: self.standa_device.get_current_position,
+                                 # 0x0301: self.standa_device.get_move_settings,
+                                 0x0302: self.standa_device.get_status}
+        self.devices_dict = {0x01: r'xi-emu:///C:/virtual_motor/virtual_motor_controller_1.bin'}
 
         self.command_format = "!HH"  # Example format: 2 bytes for command, 2 bytes for parameter
 
     def parse_command(self, packet):
-        # command, parameter = struct.unpack(self.command_format, packet)
-        # return command, parameter
-        command = packet.split()
-        if command[0] in ('0x01', '0x02'):
-            self.command_list_ids['0x0100'](command[1] if command[1] != '0x01' else r'xi-emu:///C:/virtual_motor/virtual_motor_controller_1.bin')
-
-        match command[0]:
-            case '0x01':
-                try:
-                    self.command_list_ids[command[2]](int(command[3]))
-                except ValueError:
-                    self.command_list_ids[command[2]]()
-            case '0x02':
-                self.command_list_ids[command[2]]()
+        packet = tuple(packet.split())
+        bytearray_crc = bytearray(b' '.join(packet[:-1]))
+        crc_calc = Utils.generate_CRC16(bytearray(b' '.join(packet[:-1])), 0, len(bytearray_crc))
+        crc = int(packet[-1], 16)
+        if crc != int(crc_calc.hex(), 16):
+            raise TypeError("CRC Mismatch")
+        packet_type = hex(int(packet[0], 16))
+        if packet_type in (0x01, 0x02):
+            if type(self.command_dict_ids[packet[3]]) is tuple:
+                return packet[:4] + [self.command_dict_ids[packet[3]][1](packet[4])]
+        return packet[:-1]
